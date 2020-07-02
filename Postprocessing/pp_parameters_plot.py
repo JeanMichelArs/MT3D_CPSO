be5a19079e936d099e96b718a2d02a5b82ee2a7d
JeanMichelArs <jean-michel.ars@univ-brest.fr>
2020-07-02 15:35:31 +0200

    pp_parameters_plot.py: Parameter synthetic plots

--- Postprocessing/pp_parameters_plot.py
+++ Postprocessing/pp_parameters_plot.py
@@ -216,7 +216,7 @@ for ipar in np.arange(nparam):
     align_yaxis(ax1, 0, ax2, 0)
     ax2.legend(loc=0,fontsize=10)
     plt.suptitle('Parameter '+str(ipar+1)+' <Rho>:'+str(meanpar)+'$\Omega.m$ (log scale), STD:'+str(round(std_weight[ipar],2)),fontsize=12)
-    plt.savefig(folder_save + '/' + 'Parameter '+str(ipar+1)+'.png')
+    plt.savefig(folder_save + '/' + 'Parameter_'+str(ipar+1)+'.png')
 
 
 # JOINTPLOT
