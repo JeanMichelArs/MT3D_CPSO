4f0a143d55ed0fc0678457803d07359e5cd561bb
jeremy collin <jeremy.collin@univ-brest.fr>
2020-09-18 15:00:03 +0200

    1D analytique version

--- 1D/Multiple_runs/MT1D_analytic_CPSO.py
+++ 1D/Multiple_runs/MT1D_analytic_CPSO.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python2.7
 # ----------------------------------------------------------------------------
-# CPSO for MT1D problems 
+# CPSO MT1D with analytic forward modelling for comparison to Tarits 
+# Monte Carlo Code
 #
 # For multiple runs python takes an input argument 
 # > mpirun -np $nprocs python MT1D_CPSO_chained.py $irun
-#
 # where irun is an integer referring to the number of the run
 # output files will be written in outdir
+
 # ----------------------------------------------------------------------------
 
 import sys
@@ -14,7 +15,7 @@ sys.path.append('../../Forward_MT/')
 import numpy as np
 import linecache
 import matplotlib.pyplot as plt
-import mackie3d
+#import mackie3d
 from scipy.interpolate import griddata,interp1d,Rbf
 import utm
 from stochopy import MonteCarlo, Evolutionary
@@ -24,10 +25,53 @@ import os
 import seaborn as sns
 from netCDF4 import Dataset
 
+
 # ----------------------------------------------------------------------------
-outdir = '/postproc/COLLIN/MTD3/Bolivia_1D_8param/test'
-irun = sys.argv[1]
+def MT1D_analytic(thick,rho,per):
+    if len(thick)==len(rho):
+        thick=thick[0:-1]
+
+    nlay=len(rho)
+    frequencies = 1/per
+    amu=4*np.pi*10**(-7) #Magnetic Permeability (H/m)
+    Z=np.empty(len(per),dtype=complex)
+    arho=np.empty(len(per))
+    phase=np.empty(len(per))   
+    for iff,frq in enumerate(frequencies):
+        nlay=len(rho)
+        w =  2*np.pi*frq       
+        imp = list(range(nlay))
+        #compute basement impedance
+        imp[nlay-1] = np.sqrt(w*amu*rho[nlay-1]*1j)
+        for j in range(nlay-2,-1,-1):
+            rholay = rho[j]
+            thicklay = thick[j]
+            # 3. Compute apparent rholay from top layer impedance
+            #Step 2. Iterate from bottom layer to top(not the basement) 
+            # Step 2.1 Calculate the intrinsic impedance of current layer
+            dj = np.sqrt((w * amu * (1/rholay))*1j)
+            wj = dj * rholay
+            ej = np.exp(-2*thicklay*dj)
+            #The next step is to calculate the reflection coeficient (F6) and impedance (F7) using the current layer intrinsic impedance and the prior computer layer impedance j+1.
+            belowImp = imp[j+1]
+            rj = (wj - belowImp)/(wj + belowImp)
+            re = rj*ej 
+            Zj = wj * ((1 - re)/(1 + re))
+            imp[j] = Zj
+    
+        #Finally you can compute the apparent rholay F8 and phase F9 and print the resulting data!
+        Z[iff] = imp[0]
+        absZ = abs(Z[iff])
+        arho[iff] = (absZ * absZ)/(amu * w)
+        phase[iff] = np.arctan2(np.imag(Z[iff]), np.real(Z[iff]))*180/np.pi
+        #if convert to microvolt/m/ntesla
+        Z[iff]=Z[iff]/np.sqrt(amu*amu*10**6)
+
+    return Z,arho,phase
+
+
 # ----------------------------------------------------------------------------
+
 comm = MPI.COMM_WORLD
 nproc = comm.Get_size()
 rank = comm.Get_rank()
@@ -41,22 +85,27 @@ if rank==0:
 cst_lower = 2 
 cst_upper = 2
 
+# ----
+outdir = '/postproc/COLLIN/MTD3/Analytic_1D_Bolivia_8nz/test'
+irun = sys.argv[1]
+
+
 # DECLARE VARIABLE FOR MPI
 #-------------------------
-filed = None
-popsize = None
-max_iter = None
-rho = None
-hx = None
-hy = None
-hz = None
-perpy = None
-mod1D = None
-z = None
-Erz = None
-nz = None
-rhosynth = None
-FLAGS = None
+filed=None
+popsize=None
+max_iter=None
+rho=None
+hx=None
+hy=None
+hz=None
+per=None
+mod1D=None
+z=None
+Erz=None
+nz=None
+rhosynth=None
+FLAGS=None
 
 if rank==0:
     # INITIALIZE RESISTIVITY MODEL & MACKIE INPUT
@@ -64,27 +113,18 @@ if rank==0:
     #Read 1D model
     conf_path = '../../Config/1D'
     filed = conf_path + '/mod1D_Bolivia_001'#raw_input("Initial 1D MT model:")
-    hz,rhosynth = np.loadtxt(filed,unpack=True)
-    nz = len(hz)
-    hx = np.array([10000])#np.ones([5])*1000
-    hy = np.array([10000])#np.ones([5])*1000
-    nx = 1
-    ny = 1
+    hz,rhosynth=np.loadtxt(filed,unpack=True)
+    nz=len(hz)
+    hx=np.array([10000])#np.ones([5])*1000
+    hy=np.array([10000])#np.ones([5])*1000
+    nx=1
+    ny=1
     # CPSO parameter
-    popsize = 18 #input("Size of model SWARM:")
-    max_iter = 100 *nz #input("Number of iteration:")
+    popsize = 16#input("Size of model SWARM:")
+    max_iter = 100 * nz #input("Number of iteration:")
     #Periods
-    perpy = np.loadtxt(conf_path + '/per',skiprows=1)
-    nper = len(perpy)
-    #Create 1D bottom
-    mod1D = np.loadtxt(conf_path + '/ini1d',skiprows=1)
-    n1d = len(mod1D)
-    # FORTRAN FORMAT ARRAYS
-    mod1D = np.asfortranarray(mod1D)
-    perpy = np.asfortranarray(perpy)
-    hx = np.asfortranarray(hx)
-    hy = np.asfortranarray(hy)
-    hz = np.asfortranarray(hz)
+    per=np.loadtxt(conf_path + '/per',skiprows=1)
+    nper=len(per)
     # COMPUTE MT1D DATA
     #---------------------
     print ' '
@@ -94,83 +134,42 @@ if rank==0:
     print ' -----------------'
     print ' #################'
     print ' '
-    mod1D = np.asfortranarray(mod1D)
-    perpy = np.asfortranarray(perpy)
-    resistpy = np.asfortranarray(rhosynth)
-    hx = np.asfortranarray(hx)
-    hy = np.asfortranarray(hy)
-    hz = np.asfortranarray(hz)
-    # CALL MACKIE
-    hxsurf, hysurf, hzsurf, exsurf, eysurf = mackie3d.mtd3(mod1D, perpy,
-                                                           resistpy,hx,hy,hz)
-    ###########################
-    #                         #
-    #       MT1D TENSOR       #
-    #                         #
-    ###########################
-    fmu = 4 * np.pi * 10**(-4)
-    amu = 4 * np.pi * 10**(-7)
+    z,rho,phi=MT1D_analytic(hz,rhosynth,per)
     # NOISE & ERROR LEVEL IN %
     #------------------------#
-    noise = 0.005
-    error = 0.05
+    noise=0.005
+    error=0.05
     #------------------------#
-    per = perpy
-    hxi = hxsurf[:,:,0]
-    hyi = hysurf[:,:,0]
-    hzi = hzsurf[:,:,0]
-    exi = exsurf[:,:,0]
-    eyi = eysurf[:,:,0]
-    # COMPUTE IMPEDANCE TENSOR
-    #-------------------------
-    # mackie en -iomegat, donc partie conjuguee des champs pour etre
-    # meme cadran que 2d, data,...
-    hxc = np.conj(hxi)   # (per,pol,site)
-    hyc = np.conj(hyi)
-    hzc = np.conj(hzi)
-    exc = np.conj(exi)
-    eyc = np.conj(eyi)
-    # Determinant
-    FLAGS = 1
-    det = FLAGS * (hxc[:, 0]*hyc[:, 1] - hxc[:, 1]*hyc[:, 0])
-    det = -1*(hxc[:,0] * hyc[:,1] - hxc[:,1] * hyc[:,0])
-    # ANTI-DIAG TERM => Zyx
-    z = hyc[:,1] * eyc[:,0] - hyc[:,0] * eyc[:,1]
-    z = -z / (det * fmu)
-    # Adding noise to Zyx
-    Erz = error * np.abs(z) #np.random.normal(0,error,len(zyx))*np.abs(zyx)+1e-6
-    Rz = np.real(z) + np.random.normal(0, noise, len(z)) * np.abs(z)
-    Iz = np.imag(z) + np.random.normal(0, noise, len(z)) * np.abs(z)
-    # Roayx & Phiyx
-    zt = np.abs(z)
-    rho = zt * zt * amu * 10**6 / (2. * np.pi / per)
-    rho = 10**(np.log10(rho) + np.random.normal(0, noise * np.mean(abs(np.log10(rho))),len(z)))
-    Erho = 10**(np.random.normal(0, error * np.mean(abs(np.log10(rho))), len(z)))
-    phi = np.arctan2(np.imag(z), np.real(z)) * 180 / np.pi
-    phi = phi + np.random.normal(0, noise * np.mean(abs(phi)), len(z))
-    Ephi = np.random.normal(0, error * np.mean(abs(phi)), len(z))
+    # Adding noise to Z
+    Erz=error*np.abs(z)
+    Rz=np.real(z)+np.random.normal(0,noise,len(z))*np.abs(z)
+    Iz=np.imag(z)+np.random.normal(0,noise,len(z))*np.abs(z)
+    # Rho & Phi
+    rho=rho+np.random.normal(0,noise,len(z))*abs(rho)
+    Erho=error*abs(rho)
+    phi=phi+np.random.normal(0,noise,len(z))*np.abs(phi)
+    Ephi=error*np.abs(phi)
     # WRITE DATA FILE FORMAT *.ro11, *.ro12, *.ro21, *.ro22
     #-----------------------------------------------------
-    idd = '001'    
-    file = conf_path + '/' + idd + '.ro'
+    idd='001'    
+    file= conf_path + '/' + idd + '.ro'
     #WRITE DATA
-    np.savetxt(file, np.transpose((per, Rz, Iz, Erz, rho, Erho, phi, Ephi)), 
-               fmt= '%16.10f',delimiter='     ')
+    np.savetxt(file,np.transpose(np.vstack((per,Rz,Iz,Erz,rho,Erho,phi,Ephi))),fmt= '%16.10f',delimiter='     ')
 
 
 # SHARE MPI VARIABLE
 #-------------------
-hx = comm.bcast(hx, root=0)
-hy = comm.bcast(hy, root=0)
-hz = comm.bcast(hz, root=0)
-perpy = comm.bcast(perpy, root=0)
-mod1D = comm.bcast(mod1D, root=0)
-z = comm.bcast(z, root=0)
-Erz = comm.bcast(Erz, root=0)
-nz = comm.bcast(nz, root=0)
-rhosynth = comm.bcast(rhosynth, root=0)
-popsize = comm.bcast(popsize, root=0)
-max_iter = comm.bcast(max_iter, root=0)
+hx=comm.bcast(hx, root=0)
+hy=comm.bcast(hy, root=0)
+hz=comm.bcast(hz, root=0)
+per=comm.bcast(per, root=0)
+mod1D=comm.bcast(mod1D, root=0)
+z=comm.bcast(z, root=0)
+Erz=comm.bcast(Erz, root=0)
+nz=comm.bcast(nz, root=0)
+rhosynth=comm.bcast(rhosynth, root=0)
+popsize=comm.bcast(popsize, root=0)
+max_iter=comm.bcast(max_iter, root=0)
 FLAGS=comm.bcast(FLAGS, root=0)
 
 #---------------------------------#
@@ -181,8 +180,8 @@ FLAGS=comm.bcast(FLAGS, root=0)
 
 #COST FUNCTION
 def F(X):
-    rest = 10**X
-    cost = XHI2(rest)
+    rest=10**X
+    cost=XHI2(rest)
     return cost
     ###rest=np.zeros(nx*ny*nz)
     ###print model.shape, rest.shape
@@ -196,58 +195,24 @@ def F(X):
 
 #MT MISFIT
 def XHI2(X):
-    # FORTRAN FORMAT ARRAYS
-    X = np.asfortranarray(X)
-    #print ' '
-    #print ' ################'
-    #print ' ----------------'
-    #print ' CALL MACKIE F2PY'
-    #print ' ----------------'
-    #print ' ################'
-    #print ' '
-    #print mod1D.shape, perpy.shape, X.shape, hx.shape,hy.shape,hz.shape
-    hxsurf,hysurf,hzsurf,exsurf,eysurf=mackie3d.mtd3(mod1D,perpy,X,hx,hy,hz)
-    ###########################
-    #                         #
-    #       MT MISFIT         #
-    #                         #
-    ###########################
-    fmu=4*np.pi*10**(-4)
-    amu=4*np.pi*10**(-7)
-    per_c=perpy
-    XHI2=0  
-    #------------------------#
-    perd=perpy
-    hxi=hxsurf[:,:,0]
-    hyi=hysurf[:,:,0]
-    hzi=hzsurf[:,:,0]
-    exi=exsurf[:,:,0]
-    eyi=eysurf[:,:,0]
-    # COMPUTE IMPEDANCE TENSOR
-    #-------------------------
-    # mackie en -iomegat, donc partie conjuguee des champs pour etre
-    # meme cadran que 2d, data,...
-    hxc=np.conj(hxi)   # (per,pol,site)
-    hyc=np.conj(hyi)
-    hzc=np.conj(hzi)
-    exc=np.conj(exi)
-    eyc=np.conj(eyi)
-    # Determinant
-    det = FLAGS * (hxc[:, 0]*hyc[:, 1] - hxc[:, 1]*hyc[:, 0])
-    # ANTI-DIAG TERM => Zyx
-    zc=(hyc[:,1]*eyc[:,0])-(hyc[:,0]*eyc[:,1])
-    zc=-zc/(det*fmu)
-    # Roayx & Phiyx
-    zt=np.abs(zc)
-    rhoc=zt*zt*amu*10**6/(2.*np.pi/perd)
-    phic=np.arctan2(np.imag(zc),np.real(zc))*180/np.pi
+     # COMPUTE MT1D RESP
+    #---------------------
+    print ' '
+    print ' #################'
+    print ' -----------------'
+    print ' COMPUTE MT1D RESP'
+    print ' -----------------'
+    print ' #################'
+    print ' '
+    zc,rhoc,phic=MT1D_analytic(hz,X,per)
+    print 
     #---------------------------------------------------
     #COMPUTE MT MISFIT USING IMPEDANCE TENSOR COMPONENTS
     #---------------------------------------------------
     XHI2=(sum((np.real(z)-np.real(zc))**2/Erz**2)+sum((np.imag(z)-np.imag(zc))**2/Erz**2))/2
-    #print ''
-    #print 'Magnetotelluric Misfit XHI2=>',XHI2
-    #print ''
+    print ''
+    print 'Magnetotelluric Misfit XHI2=>',XHI2
+    print ''
     return XHI2
 
 
@@ -258,32 +223,30 @@ def XHI2(X):
 #                                   #
 #-----------------------------------#
 
-# -------- 
 # changing to run directory
 os.chdir(outdir)
 
 n_dim = nz
 
 Xstart = None
-if rank == 0:
-    Xstart = np.zeros((popsize, n_dim))
+if rank==0:
+    Xstart=np.zeros((popsize,n_dim))
     for i in range(popsize):
-        Xstart[i, :] = np.log10(rhosynth) + np.random.uniform(low=-cst_lower,
-                                                  high=cst_upper, size=n_dim)
-
-
+        Xstart[i, :] = np.log10(rhosynth) + np.random.uniform(low=-cst_lower, high=cst_upper, size=n_dim)
 
 Xstart = comm.bcast(Xstart, root=0)
 
-lower = np.log10(rhosynth) - np.ones(n_dim) * cst_lower 
-upper = np.log10(rhosynth) + np.ones(n_dim) * cst_upper 
+lower=np.log10(rhosynth)-np.ones(n_dim)*cst_lower 
+upper=np.log10(rhosynth)+np.ones(n_dim)*cst_upper 
 
 # Initialize SOLVER
-ea = Evolutionary(F, lower = lower, upper = upper, popsize = popsize, 
-                  max_iter = max_iter, mpi = True, snap = True)
+ea = Evolutionary(F, lower = lower, upper = upper, popsize = popsize, max_iter = max_iter, mpi = True, snap = True)
 
 # SOLVE
-xopt, gfit = ea.optimize(solver = "cpso", xstart = Xstart , sync = True)
+xopt,gfit=ea.optimize(solver = "cpso", xstart = Xstart , sync = True)
+
+
+
 
 
 if rank==0:
@@ -295,41 +258,7 @@ if rank==0:
     print ' #################'
     print ' '
     print 'RHO_BEST:',10**xopt
-    hxsurf,hysurf,hzsurf,exsurf,eysurf=mackie3d.mtd3(mod1D,perpy,10**xopt,hx,hy,hz)
-    ###########################
-    #                         #
-    #       MT MISFIT         #
-    #                         #
-    ###########################
-    fmu=4*np.pi*10**(-4)
-    amu=4*np.pi*10**(-7)
-    per_c=perpy
-    XHI2=0  
-    #------------------------#
-    perd=perpy
-    hxi=hxsurf[:,:,0]
-    hyi=hysurf[:,:,0]
-    hzi=hzsurf[:,:,0]
-    exi=exsurf[:,:,0]
-    eyi=eysurf[:,:,0]
-    # COMPUTE IMPEDANCE TENSOR
-    #-------------------------
-    # mackie en -iomegat, donc partie conjuguee des champs pour etre
-    # meme cadran que 2d, data,...
-    hxc=np.conj(hxi)   # (per,pol,site)
-    hyc=np.conj(hyi)
-    hzc=np.conj(hzi)
-    exc=np.conj(exi)
-    eyc=np.conj(eyi)
-    # Determinant
-    det=-1*((hxc[:,0]*hyc[:,1])-(hxc[:,1]*hyc[:,0]))
-    # ANTI-DIAG TERM => Zyx
-    zc=(hyc[:,1]*eyc[:,0])-(hyc[:,0]*eyc[:,1])
-    zc=-zc/(det*fmu)
-    # Roayx & Phiyx
-    zt=np.abs(zc)
-    rhoc=zt*zt*amu*10**6/(2.*np.pi/per)
-    phic=np.arctan2(np.imag(zc),np.real(zc))*180/np.pi
+    zc,rhoc,phic=MT1D_analytic(hz,10**xopt,per)
     #---------------------------------------------------
     #COMPUTE MT MISFIT USING IMPEDANCE TENSOR COMPONENTS
     #---------------------------------------------------
