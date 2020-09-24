70f9f62520f85e36cad83a449320bd02c21b87f6
jeremy collin <jeremy.collin@univ-brest.fr>
2020-09-24 11:50:38 +0200

    devpt version mcm python

---
+++ 1D/MT1D_analytic_MCM.py
@@ -0,0 +1,324 @@
+#!/usr/bin/python2.7
+# ----------------------------------------------------------------------------
+# MCM MT1D with analytic forward modelling for comparison to Tarits 
+# Monte Carlo Code
+# ----------------------------------------------------------------------------
+
+import numpy as np
+import linecache
+import matplotlib.pyplot as plt
+from scipy.interpolate import griddata,interp1d,Rbf
+import utm
+from stochopy import MonteCarlo, Evolutionary
+from time import time
+from mpi4py import MPI
+import os
+import seaborn as sns
+from netCDF4 import Dataset
+
+# ----------------------------------------------------------------------------
+def MT1D_analytic(thick, rho, per):
+    """ 
+    Forward Modelling MT1D using analytic function 
+    - inputs: 
+      thick : layer thickness
+      rho : resisistivity
+      per : period
+    
+    - outputs
+      
+    """
+    if len(thick) == len(rho):
+        thick = thick[0:-1]
+
+    nlay = len(rho)
+    frequencies = 1 / per
+    amu = 4 * np.pi * 10**(-7) #Magnetic Permeability (H/m)
+    Z = np.empty(len(per), dtype=complex)
+    arho = np.empty(len(per))
+    phase = np.empty(len(per))   
+    for iff, frq in enumerate(frequencies):
+        nlay = len(rho)
+        w =  2 * np.pi * frq       
+        imp = list(range(nlay))
+        #compute basement impedance
+        imp[nlay-1] = np.sqrt(w * amu * rho[nlay-1] * 1j)
+        for j in range(nlay-2, -1, -1):
+            rholay = rho[j]
+            thicklay = thick[j]
+            # 3. Compute apparent rholay from top layer impedance
+            # Step 2. Iterate from bottom layer to top(not the basement) 
+            # Step 2.1 Calculate the intrinsic impedance of current layer
+            dj = np.sqrt((w * amu * (1/rholay))*1j)
+            wj = dj * rholay
+            ej = np.exp(-2*thicklay*dj)
+            # The next step is to calculate the reflection coeficient (F6)
+            # and impedance (F7) using the current layer intrinsic impedance 
+            # and the prior computer layer impedance j+1.
+            belowImp = imp[j+1]
+            rj = (wj - belowImp)/(wj + belowImp)
+            re = rj*ej 
+            Zj = wj * ((1 - re)/(1 + re))
+            imp[j] = Zj
+        # Finally you can compute the apparent rholay F8 and phase F9 and 
+        #  print the resulting data!
+        Z[iff] = imp[0]
+        absZ = abs(Z[iff])
+        arho[iff] = (absZ * absZ)/(amu * w)
+        phase[iff] = np.arctan2(np.imag(Z[iff]), np.real(Z[iff]))*180/np.pi
+        #if convert to microvolt/m/ntesla
+        Z[iff] = Z[iff] / np.sqrt(amu * amu * 10**6)
+
+    return Z, arho, phase
+
+# ---------------------------------------------------------------------------
+def F(X):
+    """
+    COST FUNCTION
+    input : X resistivity model X = [m_i]i=1,M in log10 space
+    output : Err**2 
+    """
+    rest = 10**X
+    cost = XHI2(rest)
+    return cost
+# ----------------------------------------------------------------------------
+def XHI2(X):
+    """ COMPUTE MT1D Mysfit USING IMPEDANCE TENSOR COMPONENTS """
+    zc, rhoc, phic = MT1D_analytic(hz, X, per)
+    RXHI2 = (np.sum((np.real(z) - np.real(zc))**2 / Erz**2) \
+           + np.sum((np.imag(z) - np.imag(zc))**2 / Erz**2)) * 0.5
+    print ''
+    print 'Magnetotelluric Misfit XHI2=>', RXHI2
+    print ''
+    return RXHI2
+
+# ----------------------------------------------------------------------------
+
+comm = MPI.COMM_WORLD
+nprocs = comm.Get_size()
+rank = comm.Get_rank()
+
+# Initialize TIME
+starttime = time()
+
+if rank==0:
+    print 'job running on ',nprocs,' processors'
+
+# ---> run parameters
+# probabilistic param
+cst_lower = 2 
+cst_upper = 2
+
+# outputs
+folderout = '/postproc/COLLIN/MTD3/1D_MCM_ana_8param'
+outfile = folderout + '/mcm_exploration.nc'
+
+if not os.path.exists(folderout):
+    os.makedirs(folderout)
+
+
+
+# DECLARE VARIABLE FOR MPI
+#-------------------------
+filed=None
+popsize=None
+max_iter=None
+rho=None
+hx=None
+hy=None
+hz=None
+per=None
+mod1D=None
+z=None
+Erz=None
+nz=None
+rhosynth=None
+FLAGS=None
+
+if rank==0:
+    # INITIALIZE RESISTIVITY MODEL & MACKIE INPUT
+    #--------------------------------------------
+    #Read 1D model
+    conf_path = '../Config/1D'
+    filed = conf_path + '/mod1D_Bolivia_001' # raw_input("Initial 1D MT model:")
+    hz, rhosynth = np.loadtxt(filed, unpack=True)
+    nz = len(hz)
+    hx = np.array([10000])#np.ones([5])*1000
+    hy = np.array([10000])#np.ones([5])*1000
+    nx = 1
+    ny = 1
+    # CPSO parameter
+    popsize = 16 # input("Size of model SWARM:")
+    max_iter = 100 * nz # input("Number of iteration:")
+    # Periods
+    per = np.loadtxt(conf_path + '/per',skiprows=1)
+    nper = len(per)
+    # COMPUTE MT1D DATA
+    z, rho, phi = MT1D_analytic(hz, rhosynth, per)
+    # NOISE & ERROR LEVEL IN %
+    #------------------------#
+    noise = 0.005
+    error = 0.05
+    #------------------------#
+    # Adding noise to Z
+    Erz=error*np.abs(z)
+    Rz=np.real(z)+np.random.normal(0,noise,len(z))*np.abs(z)
+    Iz=np.imag(z)+np.random.normal(0,noise,len(z))*np.abs(z)
+    # Rho & Phi
+    rho=rho+np.random.normal(0,noise,len(z))*abs(rho)
+    Erho=error*abs(rho)
+    phi=phi+np.random.normal(0,noise,len(z))*np.abs(phi)
+    Ephi=error*np.abs(phi)
+    # WRITE DATA FILE FORMAT *.ro11, *.ro12, *.ro21, *.ro22
+    #-----------------------------------------------------
+    idd='001'    
+    file= conf_path + '/' + idd + '.ro'
+    #WRITE DATA
+    #np.savetxt(file,np.transpose(np.vstack((per,Rz,Iz,Erz,rho,Erho,phi,Ephi))),fmt= '%16.10f',delimiter='     ')
+
+
+# SHARE MPI VARIABLE
+#-------------------
+hx = comm.bcast(hx, root=0)
+hy = comm.bcast(hy, root=0)
+hz = comm.bcast(hz, root=0)
+per = comm.bcast(per, root=0)
+mod1D = comm.bcast(mod1D, root=0)
+z = comm.bcast(z, root=0)
+Erz = comm.bcast(Erz, root=0)
+nz = comm.bcast(nz, root=0)
+rhosynth = comm.bcast(rhosynth, root=0)
+popsize = comm.bcast(popsize, root=0)
+max_iter = comm.bcast(max_iter, root=0)
+FLAGS = comm.bcast(FLAGS, root=0)
+
+# ---------------------------------------------------------------------------
+def F(X):
+    """
+    COST FUNCTION
+    input : X resistivity model X = [m_i]i=1,M in log10 space
+    output : Err**2 
+    """
+    rest = 10**X
+    cost = XHI2(rest)
+    return cost
+# ----------------------------------------------------------------------------
+def XHI2(X):
+    """ COMPUTE MT1D Mysfit USING IMPEDANCE TENSOR COMPONENTS """
+    zc, rhoc, phic = MT1D_analytic(hz, X, per)
+    RXHI2 = (np.sum((np.real(z) - np.real(zc))**2 / Erz**2) \
+           + np.sum((np.imag(z) - np.imag(zc))**2 / Erz**2)) * 0.5
+    print ''
+    print 'Magnetotelluric Misfit XHI2=>', RXHI2
+    print ''
+    return RXHI2
+
+# ----------------------------------------------------------------------------
+# ---->  MINIMISATION OF THE COST FUNCTION USING MCM ALGORITHM 
+
+n_dim = nz
+Xstart = None
+if rank==0:
+    Xstart = np.zeros((n_dim))
+    Xstart[:] = np.log10(rhosynth) + np.random.uniform(low=-cst_lower,
+                                           high=cst_upper, size=n_dim)
+
+Xstart = comm.bcast(Xstart, root=0)
+lower = np.log10(rhosynth) - np.ones(n_dim) * cst_lower 
+upper = np.log10(rhosynth) + np.ones(n_dim) * cst_upper 
+
+# Initialize SOLVER
+mc = MonteCarlo(F, lower = lower, upper = upper, max_iter = max_iter)
+
+# SOLVE
+xopt, gfit = mc.sample(sampler = "pure", xstart=Xstart)
+
+# ---> Plot best models response
+if rank == 0:
+    print 'RHO_BEST:',10**xopt
+    zc, rhoc, phic = MT1D_analytic(hz, 10**xopt, per)
+    #---------------------------------------------------
+    #COMPUTE MT MISFIT USING IMPEDANCE TENSOR COMPONENTS
+    #---------------------------------------------------
+    XHI2=(sum((np.real(z)-np.real(zc))**2/Erz**2)+sum((np.imag(z)-np.imag(zc))**2/Erz**2))/2
+    print ''
+    print 'Magnetotelluric Best Misfit =>',XHI2
+    print ''
+    #PLOT IMPEDANCE TENSOR
+    #---------------------
+    plt.figure(figsize=(14,15))
+    plt.subplot(211)
+    plt.errorbar(per,rho,yerr=Erho, label="$Z_{1D}^{data}$",fmt='o',markersize=10,elinewidth=2.5,color='blue')
+    plt.plot(per,rhoc,label="$Z_{1D}^{resp}$",linewidth=3,color='red')
+    plt.ylim(5,300)
+    plt.yscale('log', nonposy='clip')
+    plt.xscale('log', nonposx='clip')
+    plt.xlabel('period (sec)',fontsize=40)
+    plt.ylabel('Roa (Ohm.m)',fontsize=40)
+    xtick=10**np.arange(round(np.log10(min(per)),0),round(np.log10(max(per)),0)+1)#np.array([1e-3,1e-2,1e-1,1,1e1,1e2,1e3,1e4])
+    ytick=np.array([1,1e1,1e2,1e3])
+    plt.xticks(xtick,fontsize=30)
+    plt.yticks(ytick,fontsize=30)
+    plt.grid(True,which="both",ls="-")
+    plt.legend(prop={'size':30},loc=9,ncol=4)
+    plt.subplot(212)
+    plt.errorbar(per,phi,yerr=Ephi,label="$Z_{1D}^{data}$",fmt='o',markersize=10,elinewidth=2.5,color='blue')
+    plt.plot(per,phic,label="$Z_{1D}^{resp}$",linewidth=3,color='red')
+    plt.ylim(-180.,180,)
+    xtick=10**np.arange(round(np.log10(min(per)),0),round(np.log10(max(per)),0)+1)#np.array([1e-3,1e-2,1e-1,1,1e1,1e2,1e3,1e4])
+    ytick=np.array([-180,-135,-90,-45,0,45,90,135,180])
+    plt.xticks(xtick,fontsize=30)
+    plt.yticks(ytick,fontsize=30)
+    plt.xscale('log', nonposx='clip')
+    plt.xlabel('period (sec)',fontsize=40)
+    plt.ylabel('phase(deg)',fontsize=40)
+    plt.suptitle('site '+idd,fontsize=40)
+    plt.grid(True,which="both",ls="-")
+    plt.savefig('resp_mcm'+idd+'.eps',dpi=500, bbox_inches='tight')
+    # Print time
+    print("Elapsed time: %.2f seconds" % (time() - starttime))
+
+# ---> Write output
+# creating netcdf file
+
+if rank==0:
+    if os.path.isfile(outfile):
+        os.remove(outfile)
+    n_jobs = nprocs
+    nparam = n_dim
+    print "Writting in ", outfile
+    print "models shape:", np.shape(mc.models)
+    print ""
+    print "hx:", hx
+    print "xopt", np.shape(xopt)
+    print "models", np.shape(mc.models)
+    print "energy", np.shape(mc.energy)
+    # ---> maybe a check dimension and return 0 or 1 
+    nc = Dataset(outfile, "w", format='NETCDF4')
+    # dimensions: name, size
+    nc.createDimension('max_iter', max_iter) 
+    nc.createDimension('nz', nz)
+    nc.createDimension('nparam', len(xopt))
+    nc.createDimension('n_jobs', n_jobs)
+    # Variables: name, format, shape
+    nc.createVariable('hz', 'f8', ('nz',))
+    nc.createVariable('rho_i', 'f8', ('nz',)) 
+    nc.createVariable('xopt', 'f8', ('n_jobs', 'nparam'))
+    nc.createVariable('log_xopt', 'f8', ('nparam'))
+    nc.createVariable('models', 'f8', ('n_jobs', 'nparam', 'max_iter'))
+    nc.createVariable('energy', 'f8', ('n_jobs', 'max_iter'))  
+    nc.createVariable('rho_opt', 'f8', ('n_jobs', 'nz')) 
+    # Filling values
+    nc.variables['hz'][:] = hz
+    nc.close()
+
+# ---> each procs writes mcm outputs
+comm.Barrier()
+
+nc = Dataset(outfile, "r+", format='NETCDF4')
+nc.variables['rho_opt'][rank, :] = 10**xopt
+nc.variables['xopt'][rank, :] = xopt
+nc.variables['models'][rank, :, :] = mc.models[:, :]
+nc.variables['energy'][rank, :] = mc.energy[:]
+nc.close()
+
