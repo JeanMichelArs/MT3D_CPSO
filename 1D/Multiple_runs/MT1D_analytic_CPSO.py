20804b81cd18ce8a71640fc9951ed3fa98f093da
jeremy collin <jeremy.collin@univ-brest.fr>
2020-10-08 15:54:40 +0200

    more functions

--- 1D/Multiple_runs/MT1D_analytic_CPSO.py
+++ 1D/Multiple_runs/MT1D_analytic_CPSO.py
@@ -29,50 +29,7 @@ import os
 import seaborn as sns
 from netCDF4 import Dataset
 
-
-# ----------------------------------------------------------------------------
-def MT1D_analytic(thick,rho,per):
-    if len(thick)==len(rho):
-        thick=thick[0:-1]
-
-    nlay=len(rho)
-    frequencies = 1/per
-    amu=4*np.pi*10**(-7) #Magnetic Permeability (H/m)
-    Z=np.empty(len(per),dtype=complex)
-    arho=np.empty(len(per))
-    phase=np.empty(len(per))   
-    for iff,frq in enumerate(frequencies):
-        nlay=len(rho)
-        w =  2*np.pi*frq       
-        imp = list(range(nlay))
-        #compute basement impedance
-        imp[nlay-1] = np.sqrt(w*amu*rho[nlay-1]*1j)
-        for j in range(nlay-2,-1,-1):
-            rholay = rho[j]
-            thicklay = thick[j]
-            # 3. Compute apparent rholay from top layer impedance
-            #Step 2. Iterate from bottom layer to top(not the basement) 
-            # Step 2.1 Calculate the intrinsic impedance of current layer
-            dj = np.sqrt((w * amu * (1/rholay))*1j)
-            wj = dj * rholay
-            ej = np.exp(-2*thicklay*dj)
-            #The next step is to calculate the reflection coeficient (F6) and impedance (F7) using the current layer intrinsic impedance and the prior computer layer impedance j+1.
-            belowImp = imp[j+1]
-            rj = (wj - belowImp)/(wj + belowImp)
-            re = rj*ej 
-            Zj = wj * ((1 - re)/(1 + re))
-            imp[j] = Zj
-    
-        #Finally you can compute the apparent rholay F8 and phase F9 and print the resulting data!
-        Z[iff] = imp[0]
-        absZ = abs(Z[iff])
-        arho[iff] = (absZ * absZ)/(amu * w)
-        phase[iff] = np.arctan2(np.imag(Z[iff]), np.real(Z[iff]))*180/np.pi
-        #if convert to microvolt/m/ntesla
-        Z[iff]=Z[iff]/np.sqrt(amu*amu*10**6)
-
-    return Z,arho,phase
-
+from forward_1d import MT1D_analytic
 
 # ----------------------------------------------------------------------------
 # ---> mpi init
