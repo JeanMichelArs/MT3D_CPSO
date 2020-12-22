129fda5881c318270c3d281107c045f3db9ba057
jeremy collin <jeremy.collin@univ-brest.fr>
2020-12-22 11:03:20 +0000

    fake commit: last commit huge low_mem modifications

---
+++ SENSITIVITY_ANALYSIS/Auto/multi_cp_files.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Automatic copy of CPSO files for multiple runs with same configuration
+# Purpose is to avoid CPSO premature convergence and enhance model space 
+# exploration
+#  
+# > ./multi_cp_files.sh
+
+source /usr/share/Modules/3.2.10/init/bash
+
+# ----------------------------------------------------------------------------
+# controlled by Makefile
+
+n_runs=20
+RUN_DIR=/home2/scratch/jcollin/MT3D_CPSO/bolivia_24param_mbest_zoom
+CONF_DIR=/home2/datahome/jcollin/MT3D_CPSO/Config/bolivia_24param
+MACKIE=/home2/datahome/jcollin/MT3D_CPSO/mackie3d.so
+#------------------------------------------------------------------------------
+# local variable
+
+if [ ! -d $RUN_DIR ]; then
+  mkdir $RUN_DIR
+fi
+
+for i in $(seq 1 $n_runs)
+do
+  # ---> create mulitple directories
+  if [ ! -d $RUN_DIR/run_$i ]; then 
+    echo "creating $RUN_DIR/run_$i"
+    mkdir $RUN_DIR/run_$i 
+  fi
+  # ---> copy configuration files, mackie, *.pbs and *.py
+  echo "cp -f -t ${RUN_DIR}/run_$i/ run_[0-9]*.pbs mt3d_[0-9]*.py"
+  echo " cp -Rf ${CONF_DIR}/* ${RUN_DIR}/run_$i"
+  echo "cp -f ${MACKIE} ${RUN_DIR}/run_$i"
+  cp -f -t ${RUN_DIR}/run_$i/ run_[0-9]*.pbs mt3d_[0-9]*.py
+  cp -Rf ${CONF_DIR}/* ${RUN_DIR}/run_$i
+  cp -f ${MACKIE} ${RUN_DIR}/run_$i
+done
